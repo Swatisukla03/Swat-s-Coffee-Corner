565f97e8712eb0fff80bae1891a2ea925fe2f545
Swati <swatisukla16@gmail.com>
2022-06-06 00:00:35 +0530

    modified project

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // media query
 // @MediaCapabilities(max-widtgh)
-let navbar=document.querySelector('.navbar');
-document.querySelector('#menu-btn').onclick=()=>{
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
-let searchForm=document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick=()=>{
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
-    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
     cartItem.classList.remove('active');
-    
-
 }
 
-let cartItem=document.querySelector('.cart-items-container');
-document.querySelector('#cart-btn').onclick=()=>{
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
 }
 
-window.onscroll =() =>{
+window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
-
 }
